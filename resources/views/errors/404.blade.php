30ea9f653f404b27f2b8e84d7752a1cad2eae80f
psy <me@pys.im>
2022-06-13 13:57:00 +0800

    first commit

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <script src="{{ asset('vendor/lottie/app.js') }}" defer></script>
+
+</head>
+
+<body class="antialiased">
+    <h2 class="display-3">404</h2>
+    <p class="display-5">Oops! Something is wrong.</p>
+    <x-lottie-hello />
+</body>
+
+</html>
